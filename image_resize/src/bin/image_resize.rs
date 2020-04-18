1d048251a214b62c729bb805e2c4f92a0bdbda45
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2020-04-18 02:24:05 -0700

    added simple image resizer and color blanker tool

---
+++ image_resize/src/bin/image_resize.rs
@@ -0,0 +1,112 @@
+extern crate image;
+extern crate getopts;
+use image::GenericImageView;
+use image::GenericImage;
+use std::env;
+use std::path::PathBuf;
+use std::ffi;
+
+fn modify(img: &mut image::DynamicImage) -> Result<(), image::ImageError>{
+    let (width, height) = img.dimensions();
+    for x in 0..width {
+        for y in 0..height {
+            let pixel = img.get_pixel(x,y);
+            let image::Rgba(data) = pixel;
+            if [data[0], data[1], data[2]] == [0xe7, 0xb2, 0x34] {
+                img.put_pixel(x,y, image::Rgba([0x00,0x00,0x08,0xff]));
+            }
+        }
+    }
+    Ok(())
+}
+fn shrink_nearest(img: &image::DynamicImage) -> Result<image::RgbaImage, image::ImageError>{
+    let (width, height) = img.dimensions();
+    let small_img = image::ImageBuffer::from_fn(width>>1, height>>1, |x, y| {
+        img.get_pixel(x<<1, y<<1)
+    });
+    Ok(small_img)
+}
+fn process(input_path: &str, opt_crop: Option<Rect>) -> Result<(), image::ImageError>{
+    let path = PathBuf::from(input_path.clone());
+    let mut img = image::open(input_path)?;
+    if let Some(crop) = opt_crop {
+        img = img.crop(crop.x, crop.y, crop.w, crop.h);
+    }
+    let ext = if let Some(iext) = path.extension().clone() {
+        Some(iext.clone())
+    } else {
+        None
+    };
+    let path_nox = path.with_extension("");
+    if let Some(filename) = path_nox.file_name() {
+        let output_filename = filename.to_string_lossy().into_owned() + "_out";
+        let output_path_nox = path_nox.with_file_name(output_filename);
+        let output_path = if let Some(uext) = ext {
+            output_path_nox.with_extension(uext)
+        } else {
+            output_path_nox
+        };
+        modify(&mut img)?;
+        img.save(output_path)?;
+        let output2_filename = filename.to_string_lossy().into_owned() + "_sml";
+        let output2_path_nox = path_nox.with_file_name(output2_filename);
+        let output2_path = if let Some(uext) = ext {
+            output2_path_nox.with_extension(uext)
+        } else {
+            output2_path_nox
+        };
+        shrink_nearest(&img)?.save(output2_path)?
+    }
+    Ok(())
+}
+
+#[derive(Clone)]
+struct Rect {
+    x:u32,
+    y:u32,
+    w:u32,
+    h:u32,
+}
+impl Rect {
+    pub fn try_new(x:Option<u32>, y:Option<u32>, w_opt:Option<u32>, h_opt:Option<u32>) -> Option<Rect> {
+        if let Some(w) = w_opt {
+            if let Some(h) = h_opt {
+                if w != 0 && h != 0 {
+                    return Some(Rect {
+                        x:x.unwrap_or(0),
+                        y:y.unwrap_or(0),
+                        w:w,
+                        h:h,
+                    })
+                }
+            }
+        }
+        None
+    }
+}
+fn main() {
+    let args : Vec<ffi::OsString> = env::args_os().collect();
+    let mut opts = getopts::Options::new();
+    opts.optopt("x","cropx","set crop x", "0");
+    opts.optopt("y","cropy","set crop y", "0");
+    opts.optopt("w", "cropw", "set  crop width/wideness", "0");
+    opts.optopt("t", "croph", "set  crop height/tallness", "0");
+    opts.optflag("h", "help", "print help");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => {m}
+        Err(f)  => { panic!(f.to_string())}
+    };
+    if matches.opt_present("h") {
+        print!("{}", opts.usage(&""));
+        return;
+    }
+    let crop = Rect::try_new(
+        matches.opt_get::<u32>("cropx").unwrap(),
+        matches.opt_get::<u32>("cropy").unwrap(),
+        matches.opt_get::<u32>("cropw").unwrap(),
+        matches.opt_get::<u32>("croph").unwrap(),
+    );
+    for argument in matches.free {
+        process(&argument, crop.clone()).unwrap();
+    }
+}
